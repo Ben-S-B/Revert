9915db2ba06096f9765584feb0a913e95aa05689
mrexodia <mr.exodia.tpodt@gmail.com>
2017-01-30 02:57:40 +0100

    various playability improvements

--- wServer/realm/commands/Command.cs
+++ wServer/realm/commands/Command.cs
@@ -27,10 +27,13 @@ namespace wServer.realm.commands
         private static int GetPermissionLevel(Player player)
         {
             if (player.Client.Account.Rank == 3)
+                return 3;
+            if (player.Client.Account.Rank == 2)
+                return 2;
+            if (player.Client.Account.Rank == 1)
                 return 1;
             return 0;
         }
-
 
         public bool HasPermission(Player player)
         {
---
+++ wServer/realm/commands/SetGoldCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wServer.realm.entities.player;
+
+namespace wServer.realm.commands
+{
+    //http://www.mpgh.net/forum/showthread.php?t=1022217
+    internal class SetGoldCommand : Command
+    {
+        public SetGoldCommand() : base("setgold", 1) { }
+
+        protected override bool Process(Player player, RealmTime time, string[] args)
+        {
+            if (string.IsNullOrEmpty(args[0]))
+            {
+                player.SendHelp("Usage: /setgold <gold>");
+                return false;
+            }
+            player.Manager.Database.DoActionAsync(db =>
+            {
+                var cmd = db.CreateQuery();
+                cmd.CommandText = "UPDATE `stats` SET `credits`=@cre WHERE accId=@accId";
+                cmd.Parameters.AddWithValue("@cre", args[0]);
+                cmd.Parameters.AddWithValue("@accId", player.AccountId);
+                if (cmd.ExecuteNonQuery() == 0)
+                {
+                    player.SendError("Error setting gold!");
+                }
+                else
+                {
+                    player.SendInfo("Success!");
+                }
+            });
+            return true;
+        }
+    }
+}
