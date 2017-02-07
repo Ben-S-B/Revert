8c0f5d83f270e61d1bdb6386fa346635e5248c61
mrexodia <mr.exodia.tpodt@gmail.com>
2017-02-07 13:57:45 +0100

    implemented KeyInfoRequestPacket and KeyInfoResponsePacket

---
+++ wServer/networking/cliPackets/KeyInfoRequestPacket.cs
@@ -0,0 +1,27 @@
+﻿namespace wServer.networking.cliPackets
+{
+    public class KeyInfoRequestPacket : ClientPacket
+    {
+        public int ItemType { get; set; }
+
+        public override PacketID ID
+        {
+            get { return PacketID.KEY_INFO_REQUEST; }
+        }
+
+        public override Packet CreateInstance()
+        {
+            return new KeyInfoRequestPacket();
+        }
+
+        protected override void Read(Client psr, NReader rdr)
+        {
+            ItemType = rdr.ReadInt32();
+        }
+
+        protected override void Write(Client psr, NWriter wtr)
+        {
+            wtr.Write(ItemType);
+        }
+    }
+}
---
+++ wServer/networking/svrPackets/KeyInfoResponsePacket.cs
@@ -0,0 +1,33 @@
+﻿namespace wServer.networking.cliPackets
+{
+    public class KeyInfoResponsePacket : ClientPacket
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Creator { get; set; }
+
+        public override PacketID ID
+        {
+            get { return PacketID.KEY_INFO_RESPONSE; }
+        }
+
+        public override Packet CreateInstance()
+        {
+            return new KeyInfoResponsePacket();
+        }
+
+        protected override void Read(Client psr, NReader rdr)
+        {
+            Name = rdr.ReadUTF();
+            Description = rdr.ReadUTF();
+            Creator = rdr.ReadUTF();
+        }
+
+        protected override void Write(Client psr, NWriter wtr)
+        {
+            wtr.Write(Name);
+            wtr.Write(Description);
+            wtr.Write(Creator);
+        }
+    }
+}
