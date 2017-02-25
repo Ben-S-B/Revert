630fef3fe74c51203ed53da809f83f0b927d6a1e
mrexodia <mr.exodia.tpodt@gmail.com>
2017-02-25 06:45:59 +0100

    updated to 2.27.X10.3

--- Client Updater/Form1.cs
+++ Client Updater/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Text;
 using System.Windows.Forms;
@@ -18,15 +19,34 @@ namespace Client_Updater
 
         private void c453_btn_Click(object sender, EventArgs e) => runUpdater(textBoxDomain.Text);
 
+        private void getLanguage(string id)
+        {
+            using (var wc = new WebClient())
+            {
+                wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                var contents = wc.UploadString("https://realmofthemadgodhrd.appspot.com/app/getLanguageStrings", "languageType=" + id);
+                File.WriteAllText(id + ".txt", contents, Encoding.UTF8);
+            }
+        }
+
         private void runUpdater(string ip)
         {
             if (checkBoxDownloadClient.Checked)
             {
                 labelStatus.Text = "Status: Downloading latest client...";
                 labelStatus.Update();
-                var webCli = new WebClient();
-                var clientVersion = Encoding.UTF8.GetString(webCli.DownloadData("https://realmofthemadgodhrd.appspot.com/version.txt"));
-                webCli.DownloadFile($"https://realmofthemadgodhrd.appspot.com/AssembleeGameClient{clientVersion}.swf", "client.swf");
+                using (var webCli = new WebClient())
+                {
+                    var clientVersion = Encoding.UTF8.GetString(webCli.DownloadData("https://realmofthemadgodhrd.appspot.com/version.txt"));
+                    webCli.DownloadFile($"https://realmofthemadgodhrd.appspot.com/AssembleeGameClient{clientVersion}.swf", "client.swf");
+                }
+                getLanguage("de");
+                getLanguage("en");
+                getLanguage("es");
+                getLanguage("fr");
+                getLanguage("it");
+                getLanguage("ru");
+                getLanguage("tr");
             }
 
             updater = new ClientUpdater(ip, labelStatus);
