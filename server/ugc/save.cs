24a9b431561a8460136372690f5cbd362ad56c36
mrexodia <mr.exodia.tpodt@gmail.com>
2017-02-25 06:13:48 +0100

    added /ugc/save dummy request handler + fixed newlines

---
+++ server/ugc/save.cs
@@ -0,0 +1,23 @@
+﻿#region
+
+using db;
+using MySql.Data.MySqlClient;
+using System;
+using System.Text;
+using System.Xml;
+
+#endregion
+
+
+namespace server.ugc
+{
+    internal class save : RequestHandler
+    {
+        protected override void HandleRequest()
+        {
+            //POST: guid, password, name, description, width, height, mapjm, totalObjects, totalTiles, overwrite, Filename, thumbnail, Upload
+            WriteErrorLine("Not implemented!");
+            throw new NotImplementedException();
+        }
+    }
+}
