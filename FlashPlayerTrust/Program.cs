db04cbb59ad10e84b9f8a423d5144b85159293f6
mrexodia <mr.exodia.tpodt@gmail.com>
2017-03-14 20:47:55 +0100

    added FlashPlayerTrust

---
+++ FlashPlayerTrust/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FlashPlayerTrust
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var file = new FileInfo(Path.Combine(
+                        Environment.GetFolderPath(
+                        Environment.SpecialFolder.ApplicationData),
+                        @"Macromedia\Flash Player\#Security\FlashPlayerTrust\winetrust.cfg"));
+                file.Directory.Create();
+                File.WriteAllText(file.FullName,
+                    Path.GetPathRoot(AppDomain.CurrentDomain.BaseDirectory),
+                    Encoding.UTF8);
+                MessageBox.Show("Success!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
